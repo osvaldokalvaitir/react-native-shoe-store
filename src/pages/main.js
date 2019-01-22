933207aa987670da5785ffe7e84432c514a9be4c
osvaldokalvaitir <osvaldokalvaitir@outlook.com>
2019-01-21 23:19:38 -0200

    Entendendo o estado

--- src/pages/main.js
+++ src/pages/main.js
@@ -8,6 +8,10 @@ export default class Main extends Component {
     title: "JSHunt"
   };
 
+  state = {
+    docs: []
+  };
+
   componentDidMount() {
     this.loadProducts();
   }
@@ -17,13 +21,16 @@ export default class Main extends Component {
 
     const { docs } = response.data;
 
-    console.log(docs);
+    this.setState({ docs });
   };
 
   render() {
     return (
       <View>
-        <Text>Página Main</Text>
+        <Text>Página Main:</Text>
+        {this.state.docs.map(product => (
+          <Text key={product._id}>{product.title}</Text>
+        ))}
       </View>
     );
   }
